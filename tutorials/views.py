c23aa0667af135397113422c10ce0ad0cb878a2a
Megan DeLong <46412128+meg-atron@users.noreply.github.com>
2022-07-22 20:03:13 -0600

    Update views.py

--- tutorials/views.py
+++ tutorials/views.py
@@ -24,7 +24,8 @@ class index(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'tutorials/index.html'
 
-    def get(self, request):
+  
+# def get(self, request):
         queryset = Tutorial.objects.all()
         return Response({'tutorials': queryset})
 
